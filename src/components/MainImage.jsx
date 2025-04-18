ae5920b005bb70df118498b2949b71825bc2c9d5
Fred <leferpz@gmail.com>
2025-04-18 17:08:47 -0400

    Added main image and go home button

---
+++ src/components/MainImage.jsx
@@ -0,0 +1,10 @@
+import { Image } from "@chakra-ui/react"
+
+export default function MainImage({onClick}) {
+
+
+    
+    return (
+        <Image src="src/assets/FindTheCartoon.jpg"/>
+    )
+}
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,9 +1,23 @@
 import Header from "../components/Header"
+import MainImage from "../components/MainImage"
+import { Link } from "react-router"
+import { Button, Flex } from "@chakra-ui/react"
 
 export default function GamePage() {
+    
+    function clickHandle() {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+        console.log([mouseX, mouseY]);
+    }
+
     return (
         <section>
             <Header/>
+            <MainImage onClick={clickHandle}/>
+            <Flex justifyContent="center">
+            <Link to="/"><Button _hover={{bgColor: 'blackAlpha.900', color: "whiteAlpha.900", transform: "scale(1.13)"}} margin="20px" p={6}>Go back</Button></Link>
+            </Flex>
         </section>
     )
 }
