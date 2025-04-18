218c52031f817c1aac865c19c436f1030a7cdbb4
Fred <leferpz@gmail.com>
2025-04-18 18:20:45 -0400

    Circler is now limited to one

--- src/components/MainImage.jsx
+++ src/components/MainImage.jsx
@@ -1,33 +1,9 @@
 import { Image } from "@chakra-ui/react"
 import { createElement, useState } from "react"
 
-export default function MainImage() {
-    const [circles, setCircles] = useState([])
-
-    function clickHandle(e) {
-        const newCircle = drawCircle(e.pageX, e.pageY);
-        setCircles(prev => [...prev, newCircle]);
-    }
-
-    function drawCircle(x, y) {
-        const circle = createElement("div", {style: {
-            position: "absolute",
-            left: `calc(${x}px - 20px)`,
-            top: `calc(${y}px - 20px)`,
-            backgroundColor: "black",
-            opacity: "0.6",
-            height: "40px",
-            width: "40px",
-            borderRadius: "50px"
-         }}
-        )
-        return circle
-    }
+export default function MainImage({onClick}) {
 
     return (
-        <div>
-        <Image onClick={clickHandle} src="src/assets/FindTheCartoon.jpg"/>
-        {circles}
-        </div>
+        <Image onClick={onClick} src="src/assets/FindTheCartoon.jpg"/>
     )
 }
--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -5,12 +5,42 @@ import { Button, Flex, Box } from "@chakra-ui/react"
 import { createElement, useState } from "react"
 
 export default function GamePage() {
+    const [circle, setCircle] = useState([])
+    const [charsToFind, setCharsToFind] = useState(["Big Chungus", "Wallace", "Zoidberg"])
+
+    function clickHandle(e) {
+        // draw circle
+        const newCircle = drawCircle(e.pageX, e.pageY);
+        setCircle(newCircle);
+
+        // open form
+    }
+
+    function drawCircle(x, y) {
+        const circle = createElement("div", {style: {
+            position: "absolute",
+            left: `calc(${x}px - 20px)`,
+            top: `calc(${y}px - 20px)`,
+            backgroundColor: "black",
+            opacity: "0.6",
+            height: "40px",
+            width: "40px",
+            borderRadius: "50px"
+         }}
+        )
+        return circle
+    }
+
+    function newForm(charsToFind) {
+
+    }
 
     return (
         <section>
             <Header/>
             <Box>
-                    <MainImage/>
+                <MainImage onClick={clickHandle}/>
+                {circle}
             </Box>
             <Flex justifyContent="center">
             <Link to="/"><Button _hover={{bgColor: 'blackAlpha.900', color: "whiteAlpha.900", transform: "scale(1.13)"}} margin="20px" p={6}>Go back</Button></Link>
